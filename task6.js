62f62e36ec184931ad68315479037ab08e50fe4a
Barkro48 <siwaton548@gmail.com>
2026-08-24 15:22:16 +0700

    Update task6.js

---
+++ task6.js
@@ -0,0 +1,4 @@
+//section1
+function login(inputUser, inputPass, role, isActive, age){ if (inputUser !== "admin" || inputPass !== "ce385pass") {
+    return "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง (401)";
+  }}
